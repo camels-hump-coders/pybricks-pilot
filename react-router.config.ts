b12efc933514dae0d5377e33d7ff5592d3230d92
Nowell Strite <nowell@strite.org>
2025-08-31 23:25:54 -0400

    Deploy PR preview pages

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,10 @@
 import type { Config } from "@react-router/dev/config";
 
+const basename = process.env.BASE_PATH ?? "/";
+
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  // NOTE: we now use a custom domain of pybrickspilot.org, so no need for a basename
-  // basename: process.env.GITHUB_PAGES === "true" ? "/pybricks-pilot" : "/",
-  basename: "/",
+  basename,
 } satisfies Config;
